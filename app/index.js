00b7addee0387a7d77120ff4626808709b5da98e
Viacheslav Malchenko <viacheslav999@gmail.com>
2026-02-01 20:54:29 +0300

    fix adnpoint /uploads

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ import jwt from "jsonwebtoken";
 import { requireAdmin } from "./middlewares/adminAuth.js";
 import cookieParser from "cookie-parser";
 import { sendTelegramMessage } from "./services/telegram.js";
-
+import path from "path";
 
 
 
@@ -27,6 +27,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+  "/uploads",
+  express.static(path.join(process.cwd(), "uploads"))
+);
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
