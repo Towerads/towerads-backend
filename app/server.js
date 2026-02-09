aa370707b88135c598dffffde9ff977910248540
Viacheslav Malchenko <viacheslav999@gmail.com>
2026-02-09 15:42:53 +0300

    fix(backend): mount publisher routes under /publisher

--- app/server.js
+++ app/server.js
@@ -11,6 +11,7 @@ import { corsMiddleware } from "./middlewares/cors.js";
 import adminRoutes from "./routes/admin.routes.js";
 import advertiserRoutes from "./routes/advertiser.routes.js";
 import apiRoutes from "./routes/api.routes.js";
+import publisherRoutes from "./routes/publisher.routes.js";
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use(corsMiddleware);
 app.use(adminRoutes);
 app.use(advertiserRoutes);
 app.use(apiRoutes);
+app.use("/publisher", publisherRoutes);
 
 export default app;
 
