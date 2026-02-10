4fb2d14666d0d307896992b6a059395f642a9fcf
Viacheslav Malchenko <viacheslav999@gmail.com>
2026-02-10 18:00:06 +0300

    fix admin

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import { adminLogin } from "../controllers/admin/authController.js";
 import {
   adminStats,
   adminStatsProviders,
 } from "../controllers/admin/statsController.js";
 
 const router = Router();
+
+/**
+ * POST /admin/auth/login
+ */
+router.post("/admin/auth/login", adminLogin);
 
 /**
  * GET /admin/stats
@@ -14,5 +20,4 @@ router.get("/admin/stats", adminStats);
 router.get("/admin/stats/providers", adminStatsProviders);
 
 export default router;
-export { router };
 
