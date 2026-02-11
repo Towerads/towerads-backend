d419b74d8d824b7d1e439850659aca5ce67dca17
Viacheslav Malchenko <viacheslav999@gmail.com>
2026-02-11 17:58:12 +0300

    df

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import app from "./app/server.js";
 
 const PORT = Number(process.env.PORT || 3000);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 TowerAds API running on port ${PORT}`);
 });
